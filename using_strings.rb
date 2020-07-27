7e8529e2a7ab0d886dde9bf2e8b8bd01725ec83f
Marialaina <pmarialaina@gmail.com>
2020-07-26 18:45:13 -0600

    practicing git

--- using_strings.rb
+++ using_strings.rb
@@ -1,10 +1,11 @@
- # puts "You're ugly Demond"
-# comment = "you're ugly demond!"
-# # puts comment.upcase
-#  make_it_spicy = "!!!!"
-#  final_touch = comment.upcase + make_it_spicy
-#
-#  100000.times do
-#    puts final_touch
-#
-#  end
+ puts "You're ugly Demond"
+comment = "you're ugly demond!"
+# puts comment.upcase
+ make_it_spicy = "!!!!"
+ final_touch = comment.upcase + make_it_spicy
+
+ 100000.times do
+   puts final_touch
+
+ end
+djflebnvlr
